ee8f12738f6882bc52470e0e55fe0fa923dd5ed9
ChiedozieOkoh <chiedozieokoh1@gmail.com>
2022-06-14 09:09:42 +0100

    Support connecting to database via a postgres connection uri

--- src/db_host.js
+++ src/db_host.js
@@ -7,6 +7,14 @@ export function open_db(){
    const db_port = process.env.SYN_DB_PORT;
    const db_host = process.env.SYN_DB_HOST;
    const db_database = process.env.SYN_DB_DB;
+   const db_con_str = process.env.DB_CONNECTION_STRING;
+
+   if(db_con_str){
+      const pool = new Pool({
+         db_con_str
+      });
+      return pool;
+   }
 
    const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
 
